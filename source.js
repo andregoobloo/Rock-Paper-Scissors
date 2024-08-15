81259a936dc845ac694bb260f7a4fa0e88b3fa39
andregoobloo <177800443+andregoobloo@users.noreply.github.com>
2024-08-14 20:25:34 -0400

    Add ability to play one round as well as scores for the user and computer

--- source.js
+++ source.js
@@ -8,7 +8,6 @@ let humanScore = 0;
 const getComputerChoice = function (choices) {
   return choices[Math.trunc(Math.random() * 3)];
 };
-console.log(`Computer choice: ${getComputerChoice(choices)}`);
 
 // Need to add ability to check for inputs out of range
 const getHumanChoice = function (choices) {
@@ -17,4 +16,27 @@ const getHumanChoice = function (choices) {
   );
   return choices[userChoice];
 };
-console.log(`Your choice: ${getHumanChoice(choices)}`);
+
+const playRound = function (humanChoice, computerChoice) {
+  const pcChoice = computerChoice(choices);
+  const userChoice = humanChoice(choices);
+  const winningMessage = `You win! ${userChoice} beats ${pcChoice}`;
+  if (userChoice === pcChoice) {
+    console.log("draw");
+  } else if (userChoice === "rock" && pcChoice === "scissors") {
+    console.log(winningMessage);
+    humanScore++;
+  } else if (userChoice === "paper" && pcChoice === "rock") {
+    console.log(winningMessage);
+    humanScore++;
+  } else if (userChoice === "scissors" && pcChoice === "rock") {
+    console.log(winningMessage);
+    humanScore++;
+  } else {
+    console.log(`You lose! ${pcChoice} beats ${userChoice}`);
+    computerScore++;
+  }
+};
+playRound(getHumanChoice, getComputerChoice);
+console.log(`Your score: ${humanScore}`);
+console.log(`Computer score: ${computerScore}`);
